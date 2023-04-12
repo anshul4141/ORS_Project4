cc6498a85ab98c3477b21136295ada5c0e1e61d6
anshul4141 <anshulprajapati691@gmail.com>
2023-04-12 18:48:16 +0530

    project4

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/DuplicateRecordException.java
@@ -0,0 +1,17 @@
+package com.rays.pro4.Exception;
+
+/**
+ * DuplicateRecordException thrown when a duplicate record occurred.
+ * 
+ * @author Anshul Prajapati
+ *
+ */
+
+public class DuplicateRecordException extends Exception {
+
+	public DuplicateRecordException(String msg){
+		super(msg);
+	}
+
+	
+}
